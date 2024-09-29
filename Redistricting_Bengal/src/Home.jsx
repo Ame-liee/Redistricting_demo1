f127d177762451bb67f5e52165f4dace3fefd409
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-09-29 08:51:23 -0400

    Build: install react-usa-map and add a designed  map

--- Redistricting_Bengal/src/Home.jsx
+++ Redistricting_Bengal/src/Home.jsx
@@ -1,7 +1,82 @@
+import React, { useState } from "react";
+import USAMap from "react-usa-map";
 import bengalLogo from "./assets/Bengal.svg";
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Container } from "react-bootstrap";
 
 function Home() {
+  const [stateName, setStateName] = useState("SELECT A STATE");
+  const state = {
+    AL: "Alabama",
+    AK: "Alaska",
+    AZ: "Arizona",
+    AR: "Arkansas",
+    CA: "California",
+    CO: "Colorado",
+    CT: "Connecticut",
+    DE: "Delaware",
+    DC: "District of Columbia",
+    FL: "Florida",
+    GA: "Georgia",
+    HI: "Hawaii",
+    ID: "Idaho",
+    IL: "Illinois",
+    IN: "Indiana",
+    IA: "Iowa",
+    KS: "Kansas",
+    KY: "Kentucky",
+    LA: "Louisiana",
+    ME: "Maine",
+    MD: "Maryland",
+    MA: "Massachusetts",
+    MI: "Michigan",
+    MN: "Minnesota",
+    MS: "Mississippi",
+    MO: "Missouri",
+    MT: "Montana",
+    NE: "Nebraska",
+    NV: "Nevada",
+    NH: "New Hampshire",
+    NJ: "New Jersey",
+    NM: "New Mexico",
+    NY: "New York",
+    NC: "North Carolina",
+    ND: "North Dakota",
+    OH: "Ohio",
+    OK: "Oklahoma",
+    OR: "Oregon",
+    PA: "Pennsylvania",
+    RI: "Rhode Island",
+    SC: "South Carolina",
+    SD: "South Dakota",
+    TN: "Tennessee",
+    TX: "Texas",
+    UT: "Utah",
+    VT: "Vermont",
+    VA: "Virginia",
+    WA: "Washington",
+    WV: "West Virginia",
+    WI: "Wisconsin",
+    WY: "Wyoming",
+  };
+  const mapHandler = (event) => {
+    setStateName(state[event.target.dataset.name].toUpperCase());
+  };
+  const statesCustomConfig = () => {
+    return {
+      AL: {
+        fill: "#EEE",
+        clickHandler: (event) => console.log(event.target.dataset),
+      },
+      MS: {
+        fill: "#EEE",
+        clickHandler: (event) => console.log(event.target.dataset),
+      },
+      PA: {
+        fill: "#EEE",
+        clickHandler: (event) => console.log(event.target.dataset),
+      },
+    };
+  };
   return (
     <>
       <div className="body">
@@ -17,6 +92,14 @@ function Home() {
             &nbsp; FAIRWIN
           </Navbar.Brand>
         </Navbar>
+        <Container className="map">
+          <USAMap
+            title="USA Map"
+            defaultFill="rgb(135, 135, 135)"
+            onClick={mapHandler}
+            customize={statesCustomConfig()}
+          />
+        </Container>
       </div>
     </>
   );
