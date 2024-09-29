ff108810daac53837a36799fc66ec3da092c7319
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-09-29 02:30:41 -0400

    Feat: add a branding component with a bengal logo

--- Redistricting_Bengal/src/Home.jsx
+++ Redistricting_Bengal/src/Home.jsx
@@ -1,9 +1,25 @@
-function Home(){
-    
-  return(
-      <>
-      </>
-  )
+import bengalLogo from "./assets/Bengal.svg";
+import Navbar from "react-bootstrap/Navbar";
+
+function Home() {
+  return (
+    <>
+      <div className="body">
+        <Navbar data-bs-theme="dark" className="brand">
+          <Navbar.Brand href="#home" className="text_FAIRWIN">
+            <img
+              alt=""
+              src={bengalLogo}
+              width="40"
+              height="40"
+              className="bengal"
+            />
+            &nbsp; FAIRWIN
+          </Navbar.Brand>
+        </Navbar>
+      </div>
+    </>
+  );
 }
 
 export default Home;
