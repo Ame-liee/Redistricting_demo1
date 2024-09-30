d2ee6a7105108e279e6db1daf1241fd564450c9d
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-09-30 04:38:57 -0400

    Build: downgrade versions of each module to avoid conflicts with react-usa-map

--- Redistricting_Bengal/src/main.jsx
+++ Redistricting_Bengal/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
 import Home from './Home.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <StrictMode>
     <Home />
   </StrictMode>,
-)
+  rootElement
+);
