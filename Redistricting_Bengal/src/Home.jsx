bd676651d825c1c690051afb5a6ea9a76f883a3c
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-09-30 14:43:11 -0400

    Style: change elements sizes depends on the window size

--- Redistricting_Bengal/src/Home.jsx
+++ Redistricting_Bengal/src/Home.jsx
@@ -87,37 +87,37 @@ function Home() {
         </Container>
       </div>
       <div className="body1">
-        <h2 className="text_subQuestion">
+        <h2 className="text_subQuestion1_1">
           WILL FAIR REPRESENTATION ACT(FRA) for MMD
-        </h2>
-        <span className="text_subQuestion2"> INCREASE FAIRNESS?</span>
-        <Button
-          variant="link"
-          className="button_information"
-          onClick={() => setShowInfo1(true)}
-        >
-          <svg
-            fill="rgb(40, 38, 38)"
-            version="1.1"
-            id="Capa_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="30px"
-            height="30px"
-            viewBox="0 0 416.979 416.979"
-            xml:space="preserve"
+          <span className="text_subQuestion2"> INCREASE FAIRNESS?</span>
+          <Button
+            variant="link"
+            className="button_information"
+            onClick={() => setShowInfo1(true)}
           >
-            <g>
-              <path
-                d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
+            <svg
+              fill="rgb(40, 38, 38)"
+              version="1.1"
+              id="Capa_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="30px"
+              height="30px"
+              viewBox="0 0 416.979 416.979"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
     c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786
     c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576
     c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765
     c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z"
-              />
-            </g>
-          </svg>
-        </Button>
+                />
+              </g>
+            </svg>
+          </Button>
+        </h2>
         {showInfo1 && (
           <Alert
             variant="dark"
@@ -155,40 +155,40 @@ function Home() {
         </Nav>
         <div className="graph1">temporary graph</div>
         <br />
-        <h2 className="text_subQuestion">
+        <h2 className="text_subQuestion1_2">
           WILL FAIR REPRESENTATION ACT(FRA) for MMD
-        </h2>
-        <span className="text_subQuestion2">
-          {" "}
-          LESSEN THE GERRYMANDERING EFFECTS?
-        </span>
-        <Button
-          variant="link"
-          className="button_information"
-          onClick={() => setShowInfo2(true)}
-        >
-          <svg
-            fill="rgb(40, 38, 38)"
-            version="1.1"
-            id="Capa_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="30px"
-            height="30px"
-            viewBox="0 0 416.979 416.979"
-            xml:space="preserve"
+          <span className="text_subQuestion2">
+            {" "}
+            LESSEN THE GERRYMANDERING EFFECTS?
+          </span>
+          <Button
+            variant="link"
+            className="button_information"
+            onClick={() => setShowInfo2(true)}
           >
-            <g>
-              <path
-                d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
+            <svg
+              fill="rgb(40, 38, 38)"
+              version="1.1"
+              id="Capa_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="30px"
+              height="30px"
+              viewBox="0 0 416.979 416.979"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
     c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786
     c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576
     c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765
     c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z"
-              />
-            </g>
-          </svg>
-        </Button>
+                />
+              </g>
+            </svg>
+          </Button>
+        </h2>
         {showInfo2 && (
           <Alert
             variant="dark"
