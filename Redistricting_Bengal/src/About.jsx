66e1a4e3dc1b73bf0be14cab818e1ef03b420be2
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-10-01 05:17:53 -0400

    Chore: revise the about contents

--- Redistricting_Bengal/src/About.jsx
+++ Redistricting_Bengal/src/About.jsx
@@ -112,8 +112,19 @@ function About() {
             2. Will the Fair Representation Act for multi-member election
             districts (MMD) lessen the effects of Gerrymandering?{" "}
           </p>
+          <br />
           <h1>ABOUT Fair Representation Act</h1>
+          <p>1. Multi-member districts</p>
+          <p>2. Ranked choice voting</p>
+          <br />
+          <h1>ABOUT Data</h1>
+          <p>2020 House of Representative Election</p>
+          <p>Reference : </p>
+          <br />
           <h1>ABOUT Bengal</h1>
+          <p>Data: Hyunjun Cho, HyoJong Chung</p>
+          <p>BackEnd: Sinae Hong, Hyunjun Cho</p>
+          <p>FrontEnd: Yeonkyung Ha</p>
         </div>
       </div>
     </>
