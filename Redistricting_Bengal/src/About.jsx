599b9dc5ecdee7083b456b78603e04afd685ad6b
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-10-01 05:00:51 -0400

    Feat: add about content

--- Redistricting_Bengal/src/About.jsx
+++ Redistricting_Bengal/src/About.jsx
@@ -3,7 +3,21 @@ import React from "react";
 function About() {
   return (
     <>
-      <h1>Hi</h1>
+    <div className="body">
+        <div className="text_aboutContent">
+      <h1>ABOUT FAIRWIN project</h1>
+      <p>
+        1. Will the Fair Representation Act for multi-member election districts
+        (MMD) increase fairness?{" "}
+      </p>
+      <p>
+        2. Will the Fair Representation Act for multi-member election districts
+        (MMD) lessen the effects of Gerrymandering?{" "}
+      </p>
+      <h1>ABOUT Fair Representation Act</h1>
+      <h1>ABOUT Bengal</h1>
+      </div>
+      </div>
     </>
   );
 }
