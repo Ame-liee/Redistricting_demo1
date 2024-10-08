6aaae18ecbc75a105ba3f8795338a64a1dbf6d9f
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-10-08 14:13:48 -0400

    Feat: show the right arrow button only when an available state selected

--- Redistricting_Bengal/src/Home.jsx
+++ Redistricting_Bengal/src/Home.jsx
@@ -442,11 +442,7 @@ function Home() {
                 <span className="char16">*</span>
               </h1>
               <Nav className="sidebar_nav">
-                <Nav.Link
-                  href="/"
-                >
-                  STATE SELECTION
-                </Nav.Link>
+                <Nav.Link href="/">STATE SELECTION</Nav.Link>
                 {/* <NavDropdown title="ANALYSIS" className="sidebar_dropdown">
                   <NavDropdown.Item
                     className="sidebar_dropdownItem"
@@ -530,30 +526,35 @@ function Home() {
                 })}
               </svg>
             </Container>
-            <div className="button_toAnalysis">
-              <Button variant="link" onClick={() => toAnalysis(selectedState)}>
-                <svg
-                  width="50px"
-                  height="50px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+            {customStates.includes(selectedState) && (
+              <div className="button_toAnalysis">
+                <Button
+                  variant="link"
+                  onClick={() => toAnalysis(selectedState)}
                 >
-                  <path
-                    d="M3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
-                    stroke="rgb(255, 255, 255)"
-                    strokeWidth="2"
-                  />
-                  <path
-                    d="M8 12H16M16 12L12 16M16 12L12 8"
-                    stroke="rgb(255, 255, 255)"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </Button>
-            </div>
+                  <svg
+                    width="50px"
+                    height="50px"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                      stroke="rgb(255, 255, 255)"
+                      strokeWidth="2"
+                    />
+                    <path
+                      d="M8 12H16M16 12L12 16M16 12L12 8"
+                      stroke="rgb(255, 255, 255)"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Button>
+              </div>
+            )}
             <div className="dataExplaination">
               <svg
                 width="10px"
