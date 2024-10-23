2f49d9c7f58cb1bfc82328f87940355b5321d2a3
Yeonkyung Ha <80248321+yeonkyung-ha@users.noreply.github.com>
2024-10-23 04:31:43 -0400

    Build: add Ensemble.jsx

--- Redistricting_Bengal/src/Ensemble.jsx
+++ Redistricting_Bengal/src/Ensemble.jsx
@@ -8,6 +8,7 @@ import congDist from "./assets/blank_ensemble.json";
 import copyGeo from "./assets/copyGeo.json";
 import Sidebar from "./Components/Sidebar";
 import Brand from "./Components/Brand";
+import BoxWhisker from "./Components/BoxWhisker";
 import { MapContainer, GeoJSON } from "react-leaflet";
 import {
   BarChart,
@@ -39,37 +40,6 @@ import {
   Button,
   // Carousel,
 } from "react-bootstrap";
-
-// Horizontal Line
-const HorizonBar = (props) => {
-  const { x, y, width, height } = props;
-  if (x == null || y == null || width == null || height == null) {
-    return null;
-  }
-  return (
-    <line x1={x} y1={y} x2={x + width} y2={y} stroke={"#000"} strokeWidth={3} />
-  );
-};
-
-// Whisker
-const DotBar = (props) => {
-  const { x, y, width, height } = props;
-
-  if (x == null || y == null || width == null || height == null) {
-    return null;
-  }
-  return (
-    <line
-      x1={x + width / 2}
-      y1={y + height}
-      x2={x + width / 2}
-      y2={y}
-      stroke={"#000"}
-      strokeWidth={5}
-      strokeDasharray={"5"}
-    />
-  );
-};
 
 // BoxPlot
 const useBoxPlot = (boxPlots) => {
@@ -496,116 +466,19 @@ function Ensemble() {
                     className="item_contents_analysis"
                     style={{ width: "100%", height: 330 }}
                   >
-                    <ResponsiveContainer className="responsiveContainer">
-                      <ComposedChart data={boxWhiskerSMD}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Bar stackId={"a"} dataKey={"min"} fill={"none"} />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bottomWhisker"}
-                          shape={<DotBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bottomBox"}
-                          fill={"#8884d8"}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"topBox"}
-                          fill={"#8884d8"}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"topWhisker"}
-                          shape={<DotBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <ZAxis type="number" dataKey="size" range={[0, 250]} />
-                        {/* 
-                                <Scatter
-                                  dataKey="average"
-                                  fill={"red"}
-                                  stroke={"#FFF"}
-                                /> */}
-                        <XAxis dataKey="name" />
-                        <YAxis
-                          domain={[0, 1]}
-                          tickFormatter={formatYAxisTick}
-                        />
-                      </ComposedChart>
-                    </ResponsiveContainer>
+                    <BoxWhisker
+                      data={boxWhiskerSMD}
+                      formatYAxisTick={formatYAxisTick}
+                    />
                   </Row>
                   <Row
                     className="item_contents_analysis"
                     style={{ width: "100%", height: 330 }}
                   >
-                    <ResponsiveContainer className="responsiveContainer">
-                      <ComposedChart data={boxWhiskerMMD_data}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Bar stackId={"a"} dataKey={"min"} fill={"none"} />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bottomWhisker"}
-                          shape={<DotBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bottomBox"}
-                          fill={"#8884d8"}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"topBox"}
-                          fill={"#8884d8"}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"topWhisker"}
-                          shape={<DotBar />}
-                        />
-                        <Bar
-                          stackId={"a"}
-                          dataKey={"bar"}
-                          shape={<HorizonBar />}
-                        />
-                        <ZAxis type="number" dataKey="size" range={[0, 250]} />
-                        {/* <Scatter
-                                  dataKey="average"
-                                  fill={"red"}
-                                  stroke={"#FFF"}
-                                /> */}
-                        <XAxis dataKey="name" />
-                        <YAxis
-                          domain={[0, 1]}
-                          tickFormatter={formatYAxisTick}
-                        />
-                      </ComposedChart>
-                    </ResponsiveContainer>
+                    <BoxWhisker
+                      data={boxWhiskerMMD_data}
+                      formatYAxisTick={formatYAxisTick}
+                    />
                   </Row>
                 </Container>
               )}
